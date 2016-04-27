c499e6d6c95a9dba4a1a938ce5436088e1aa3331
vunguyen <nguyen.levu@smartdev.vn>
2016-04-27 14:35:43 +0700

    fix repository

--- src/main/java/com/howtodoinjava/demo/service/EmployeeManagerImpl.java
+++ src/main/java/com/howtodoinjava/demo/service/EmployeeManagerImpl.java
@@ -3,10 +3,11 @@ package com.howtodoinjava.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.howtodoinjava.demo.dao.EmployeeJPARepository;
 import com.howtodoinjava.demo.model.EmployeeEntity;
+import com.howtodoinjava.demo.repository.EmployeeJPARepository;
 
 
 
